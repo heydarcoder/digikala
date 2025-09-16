8b0f8ee0057ff596dd7154e96a2dd3b6734367ad
heydarcoder <heydarcode@gmail.com>
2025-09-16 14:14:29 +0330

    add login and logout to my project

--- digikala_web/shop/urls.py
+++ digikala_web/shop/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("" , views.hello_world, name="hello_world"),
+    path("about/" , views.about, name="about" ),
+    path("login/" , views.login_user, name="login"),
+    path("logout/" , views.logout_user, name="logout"),
 ]
--- digikala_web/shop/views.py
+++ digikala_web/shop/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def hello_world(request):
     all_products = Product.objects.all()
@@ -8,3 +10,25 @@ def hello_world(request):
 
     return render(request , "index.html" , context)
 
+def about(request):
+    return render(request , 'about.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('hello_world')
+        else:
+            messages.success(request, 'شما وارد نشدید')
+            return redirect('hello_world')
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect("hello_world")
