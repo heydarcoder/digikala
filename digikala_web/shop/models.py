d5706a6fd4ff0b1e171befd188217aec01d1ca03
heydarcoder <heydarcode@gmail.com>
2025-09-18 01:25:44 +0330

    add cart for project and add style for pages

--- digikala_web/shop/models.py
+++ digikala_web/shop/models.py
@@ -1,13 +1,17 @@
 from datetime import datetime
 
 from django.db import models
-from django.core.validators import MinValueValidator , MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
@@ -15,14 +19,16 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     password = models.CharField(max_length=100)
+
     def __str__(self):
         return f"{self.name} {self.last_name}"
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200 , default="" ,blank=True , null=True )
-    price = models.DecimalField(default=0 , decimal_places= 0, max_digits= 12)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE , default=1)
+    description = models.CharField(max_length=200, default="", blank=True, null=True)
+    price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to="upload/products/")
     # SIZES = (
     #     ('M','32'),
@@ -31,25 +37,24 @@ class Product(models.Model):
     # )
     # size = models.CharField(max_length=1, choices=SIZES, default='32')
     is_sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(default=0 , decimal_places=0, max_digits=12)
-    star = models.IntegerField(default=0 , validators=[MinValueValidator(0), MaxValueValidator(5)])
+    sale_price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+    star = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    address = models.CharField(max_length=200 , default="" ,blank=True , null=True )
-    phone = models.CharField(max_length=20 , blank=True , null=True )
+    address = models.CharField(max_length=200, default="", blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField()
     date = models.DateTimeField(default=datetime.today())
     status = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.customer} {self.product} {self.quantity}"
-
-
-
-
-
